c7de1fa92ada2835dc7cc59d47834eca47019e39
Weny001 <60356564+weny001@users.noreply.github.com>
2020-03-09 08:42:48 +0000

    Create wasd_movement.cs

---
+++ UnityC#/wasd_movement.cs
@@ -0,0 +1,19 @@
+ void Update ()
+ {
+     if (Input.GetKey(KeyCode.LeftArrow))
+     {
+         transform.position += Vector3.left * speed * Time.deltaTime;
+     }
+     if (Input.GetKey(KeyCode.RightArrow))
+     {
+         transform.position += Vector3.right * speed * Time.deltaTime;
+     }
+     if (Input.GetKey(KeyCode.UpArrow))
+     {
+         transform.position += Vector3.up * speed * Time.deltaTime;
+     }
+     if (Input.GetKey(KeyCode.DownArrow))
+     {
+         transform.position += Vector3.down * speed * Time.deltaTime;
+     }
+ }
