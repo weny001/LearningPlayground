c63561675252ebd6c0182936742fc8b015b84573
Weny001 <60356564+weny001@users.noreply.github.com>
2020-03-09 08:44:07 +0000

    Update wasd_movement.cs

--- UnityC#/wasd_movement.cs
+++ UnityC#/wasd_movement.cs
@@ -1,4 +1,14 @@
- void Update ()
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour 
+{
+    // Start is called before the first frame update
+    void Start()
+
+
+void Update ()
  {
      if (Input.GetKey(KeyCode.LeftArrow))
      {
