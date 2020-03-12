e07a314110545555dc5e2430e201ace4ace316d5
Weny001 <60356564+weny001@users.noreply.github.com>
2020-03-12 08:54:21 +0000

    Add files via upload

---
+++ UnityC#/Movement Side to side platformer.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+    private Rigidbody2d mybody;
+
+    public float speed;
+    public float jumpPower;
+
+    void start () {
+        mybody - gameObject.GetComponent<Rigidbody2d>();
+    }
+
+    void Update () {
+        float move = Input.GetAxis("Horizontal");
+        mybody.velocity = new Vector2(move * speed, mybody.velocity.y);
+
+        if(Input.GetKey(KeyCode.Space)) {
+            anybody.velocity = new Vector2(mybody.velocity.x, jumpPower);
+        }
+    }
+}
